556605ab9590d8c550d4f24e760f758389b3f6e3
Albert Huynh <alberthuynh91@gmail.com>
2019-02-01 22:24:47 -0800

    Transpose matrix solution

---
+++ transpose-matrix.js
@@ -0,0 +1,13 @@
+var transpose = function(A) {
+  const transposed = A[0].map((item) => {
+    return []
+  })
+  for (var row = 0; row < A.length; row++) {
+      for (var col = 0; col < A[0].length; col++) {
+          transposed[col].push(A[row][col])
+      }
+  }
+  return transposed
+};
+
+transpose([[1,2,3],[4,5,6],[7,8,9]])
