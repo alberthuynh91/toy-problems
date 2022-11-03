b64546aaf5dad25d90be0808a1412fef00b1c831
Albert Huynh <alberthuynh91@gmail.com>
2022-11-03 11:25:40 -0700

    Update with recursion solution

--- palindrome.js
+++ palindrome.js
@@ -10,3 +10,19 @@ var isPalindrome = function(s) {
     }
     return true
 };
+
+
+// Recursion solution
+const isPalindrome = (s) => {
+  // Base-case / stopping condition
+  if (s.length === 0 || s.length === 1) {
+    return true
+  }
+  
+  // Do some work to shrink the problem space
+  if (s[0] === s[s.length - 1]) {
+    // If the two outer letters are equal, remove them and check the next outer letters
+    const shrunkenString = s.substring(1, s.length - 1)
+    return isPalindrome(shrunkenString)
+  }
+}
