71dcd71f986a0ffec1af50d589d35125a88e3d1a
Albert Huynh <alberthuynh91@gmail.com>
2022-11-08 10:09:05 -0800

    Update transpose-matrix.js

--- transpose-matrix.js
+++ transpose-matrix.js
@@ -9,3 +9,26 @@ var transpose = function(A) {
 };
 
 transpose([[1,2,3],[4,5,6],[7,8,9]])
+
+// Updated solution 11/8/22
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+
+var transpose = function(matrix) {
+    const transposedArr = matrix[0].map(() => [])
+    for (var i = 0; i < matrix.length; i++) { // rows
+        for (var j = 0; j < matrix[i].length; j++) { // cols
+            transposedArr[j][i] = matrix[i][j]
+        }
+    }
+    return transposedArr
+};
+
+
+// Another way to create empty 2d array 
+// let transposedArr = Array.from({ length: matrix[0].length}, () => new Array(matrix.length).fill(0))
+
+// Hint: The indices are flipped when transposing
